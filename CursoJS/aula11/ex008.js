a291b559f3413e6e1e527bb4a9d86e50a0f89ab5
Thiago Utsch <83710504+thiagoutsch@users.noreply.github.com>
2024-02-22 19:54:53 -0300

    Add files via upload

---
+++ CursoJS/aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 60.5
+console.log (`a velocidade do seu carro é ${vel}km/h`)
+if (vel>70) {
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+} else {
+    console.log('Continue dirigindo na velocidade permitida! Parabéns')
+}
+console.log('Dirija sempre usando cinto de segurança')
---
+++ CursoJS/aula12/ex011.js
@@ -0,0 +1,8 @@
+var idade = 13
+if (idade<16){
+    console.log('Não Vota')
+} else if (idade >= 16 && idade < 18 || idade > 65) {
+        console.log('Voto opcional')
+} else {
+    console.log('Voto obrigatório')
+}
